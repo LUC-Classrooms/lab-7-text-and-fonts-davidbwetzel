38d3ba735b8b66bc974d9d42b5a651ee3928d0d8
davidbwetzel <david@wetzel-soyster.net>
2024-02-23 16:47:01 +0000

    Updated extra credit

--- script.js
+++ script.js
@@ -7,17 +7,22 @@ function setup() {
   textFont("Courier");
 }
 
+var phrase1 = "Nobody goes there any more ...";
+var phrase2 = "it's too crowded!";
+var phrase = ""; 
+
 function draw() {
   background(200);
   if(mouseIsPressed){
     textFont("Courier");
     textSize(32);
-    text("It's too crowded!", 20, 100);
+    phrase = phrase2;
   } else {
     textSize(18);
     textFont("Times");
-    text("Nobody goes there anymore ...", 20, 100);  
+    phrase = phrase1;
   }
+  text(phrase, 20, 100);
 
 
 }
